26313c61271460657ce7ceece8f466970d3381c3
kishism <morrg25@gmail.com>
2025-07-20 02:15:29 +0630

    Remove all node.js components

--- models/AdminUser.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminUserSchema = new mongoose.Schema({
-  username: String,
-  password: String, // Hashed password
-});
-
-module.exports = mongoose.model('AdminUser', AdminUserSchema);
